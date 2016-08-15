4552f1215c5954b29bb3f190eeff0575b8e60f39
Dang Quoc Huy <huysamdua@yahoo.com>
2016-08-15 10:06:26 +0700

    Step 1: Send your first notification

---
+++ PonkemoGo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  PonkemoGo
+//
+//  Created by Dang Quoc Huy on 8/12/16.
+//  Copyright © 2016 Dang Quoc Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pokemon1Button: UIButton!
+    @IBOutlet weak var pokemon2Button: UIButton!
+    @IBOutlet weak var pokemon3Button: UIButton!
+    
+    var pokemons: [UIButton] = []
+    var pokemonNames = ["Pikachu", "Dragon", "Dinosaur"]
+    var selectedIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        pokemons.append(pokemon1Button)
+        pokemons.append(pokemon2Button)
+        pokemons.append(pokemon3Button)
+    }
+
+    @IBAction func onSelectPokemon(sender: UIButton) {
+        
+        for (index, button) in pokemons.enumerate() {
+            if sender == button {
+                selectedIndex = index
+                activeBoder(button)
+            } else {
+                inactiveBoder(button)
+            }
+        }
+    }
+    
+    @IBAction func tapNotify(sender: AnyObject) {
+        
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 10)
+        notification.alertBody = "\(pokemonNames[selectedIndex]) is nearby"
+        notification.userInfo = ["SelectedPokemon": pokemonNames[selectedIndex]]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func activeBoder(button: UIButton) {
+        
+        button.layer.borderColor = UIColor.blueColor().CGColor
+        button.layer.borderWidth = 1
+    }
+    
+    func inactiveBoder(button: UIButton) {
+        
+        button.layer.borderWidth = 0
+    }
+}
